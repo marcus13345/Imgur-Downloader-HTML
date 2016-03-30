2676f5e27b5b94deff0d1fedb337d2492e39265e
Marcus Gosselin <marcus13345@live.com>
2016-03-29 22:28:21 -0400

    commiting for issue

--- html/main.js
+++ html/main.js
@@ -1,5 +1,7 @@
 var $ = require('jquery');
 
+document.getElementsByTagName("body")[0].style.background = 'red';
+/*
 var data = {"downloads":[
   {
     "subreddit": "emmawatson",
@@ -17,6 +19,29 @@ var data = {"downloads":[
   }
 ]};
 
+const DIVIDER = "<div id=\"divider\"></div>";
+/* structure for injecting items
+<div class="item">
+  <div class="header">
+    Emma Watson
+  </div>
+  <div class="status">
+    Scanning Page 1...
+  </div>
+</div>
+*//*
 function updateItems() {
   //print out items to downloads section thing
 }
+var UI = {
+  addItem: function (area, subreddit) {
+    //$('body')[0].css("background-color", "red");
+
+    document.getElementsByTagName("body")[0].style.background = 'red';
+  }
+}
+UI.addItem(0, 0);
+setTimeout(function() {
+
+}, 0);
+*/
