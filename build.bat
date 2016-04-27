08c1422a294c2611e3f26c6160b4ef3679069bc0
Marcus Gosselin <marcus13345@live.com>
2016-04-27 03:26:55 -0400

    added build scripts

---
+++ build.bat
@@ -0,0 +1,1 @@
+electron-packager . --overwrite --platform=win32 --arch=ia32 --icon=icons/mac.png --out=bin
---
+++ build.sh
@@ -0,0 +1,1 @@
+electron-packager . --overwrite --platform=darwin --arch=x64 --icon=icons/mac.icns --out=bin
