70885d9630b971fccdb2f2e96793d75194b2a205
Marcus Gosselin <marcus13345@live.com>
2016-04-30 23:01:55 -0400

    updated package.json

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 electron-packager . --overwrite --platform=darwin --arch=x64 --icon=icons/mac.icns --out=bin
+hdiutil create -volname Install\ SubSavr -srcfolder ./bin/SubSavr-darwin-x64 -ov -format UDZO installer.dmg
